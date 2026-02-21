4259141d5b9eec3d994b12cc779085b7a108dcf4
raj-tx-max <raj4tx@gmail.com>
2026-02-21 16:55:55 -0600

    a

--- aws-tf/IAM_create_User.tf
+++ aws-tf/IAM_create_User.tf
@@ -1,3 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket = "1terraform-state-bucket"
+    key    = "state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
 # -----------------------------
 # Creating new IAM Group admin
 # -----------------------------
@@ -50,11 +57,5 @@ resource "aws_iam_user_group_membership" "team" {
     data.aws_iam_group.existing_admin.group_name
   ]
 
-terraform {
-  backend "s3" {
-    bucket = "1terraform-state-bucket"
-    key    = "state/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+
 }
