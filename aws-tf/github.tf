438dba64f0f4dd8ed79dd80cd6bcbdf736e38f6e
raj <raj4tx@gmail.com>
2026-02-23 20:39:27 -0600

    removed version from tf block

--- aws-tf/github.tf
+++ aws-tf/github.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "~> 6.0"
+      
     }
   }
 }
 
 # Configure the GitHub Provider
 provider "github" {
-    token = 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
+   
 }
 # Create a new GitHub repository
 resource "github_repository" "my_repo" {
