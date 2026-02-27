69f51bd5c1def55b0f28209d16801621b839d419
raj-tx-max <raj4tx@gmail.com>
2026-02-27 15:19:20 -0600

    Update providers.tf

--- aws-tf/providers.tf
+++ aws-tf/providers.tf
@@ -4,12 +4,9 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
- #   github = {
- #    source  = "integrations/github"    
-#    }    
-  }
-
   }
 provider "github" {
   # Leave empty. It will grab the GITHUB_TOKEN from the environment.
     }
+  }
+
