d6efa490e518553287dcf10e233b4e4cfb92db30
raj-tx-max <raj4tx@gmail.com>
2026-02-27 17:09:38 -0600

    Update providers.tf

--- aws-tf/providers.tf
+++ aws-tf/providers.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    github = { 
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
 provider "github" {
   # Reads GITHUB_TOKEN from environment
 }
