c2c8e5e251b1e80fad1bbbd997297007044f2d67
raj-tx-max <raj4tx@gmail.com>
2026-02-27 14:38:30 -0600

    Update providers.tf

--- aws-tf/providers.tf
+++ aws-tf/providers.tf
@@ -1,22 +1,14 @@
 terraform {
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
     github = {
-      source  = "integrations/github"
-      
-    }
-    
+      source  = "integrations/github"    
+    }    
   }
-  backend "s3" {
-    bucket = "1terraform-state-bucket"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-    use_lockfile = true
-  }
+
   }
 provider "github" {
   # Leave empty. It will grab the GITHUB_TOKEN from the environment.
