04a5a12f7c1ace084d47e0197ef88da92cd9ef91
raj-tx-max <raj4tx@gmail.com>
2026-02-27 16:42:09 -0600

    Update github.tf

--- aws-tf/github.tf
+++ aws-tf/github.tf
@@ -1,7 +1,7 @@
 
 # Create a new GitHub repository
-resource "github_repository" "my_repo" {
-    name        = "my-terraform-repo"
-    description = "A repository created using Terraform"
-    visibility  = "public"
+#resource "github_repository" "my_repo" {
+#    name        = "my-terraform-repo"
+#    description = "A repository created using Terraform"
+#    visibility  = "public"
 }
