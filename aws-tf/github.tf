694b965bb7f3c50437e1fb7d4c8c00a6acc95793
raj-tx-max <raj4tx@gmail.com>
2026-02-27 16:58:37 -0600

    Delete aws-tf/github.tf

--- aws-tf/github.tf
+++
@@ -1,7 +0,0 @@
-
-# Create a new GitHub repository
-#resource "github_repository" "my_repo" {
-#    name        = "my-terraform-repo"
-#    description = "A repository created using Terraform"
-#    visibility  = "public"
-#}
