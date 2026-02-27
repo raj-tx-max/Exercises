6ac68b2b54e5684ae77b2667ae245e9c6542cf5c
raj-tx-max <raj4tx@gmail.com>
2026-02-27 14:35:53 -0600

    Update providers.tf

--- aws-tf/providers.tf
+++ aws-tf/providers.tf
@@ -13,8 +13,9 @@ terraform {
   }
   backend "s3" {
     bucket = "1terraform-state-bucket"
-    key    = "state/terraform.tfstate"
+    key    = "terraform.tfstate"
     region = "us-east-1"
+    use_lockfile = true
   }
   }
 provider "github" {
